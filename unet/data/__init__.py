fa5cc91e9842341413fefcef8d391c580653d3c1
ycremar <xieyc95@gmail.com>
2019-09-09 10:53:01 -0500

    Update __init__.py

--- unet/data/__init__.py
+++ unet/data/__init__.py
@@ -1,3 +1,3 @@
-from unet.data.input_fn import input_function, input_function_npz
+from unet.data.input_fn import input_function
 from unet.data.prepare_test import prepare_test
 from unet.data.evaluate_fn import evaluate_function
